b35923e74fe1083f4f3b4cfb899485b7c8ee1202
SergiioDev <sergi13456@hotmail.es>
2022-09-30 16:42:37 +0200

    create insert method

--- src/main/java/datastructures/linkedlists/LinkedList.java
+++ src/main/java/datastructures/linkedlists/LinkedList.java
@@ -1,5 +1,7 @@
 package datastructures.linkedlists;
 
+import org.w3c.dom.Node;
+
 public class LinkedList {
     private NodeLinkedList head;
     private NodeLinkedList tail;
@@ -25,6 +27,27 @@ public class LinkedList {
         length++;
     }
 
+    public void insert (int index, int data){
+        NodeLinkedList preNode = head;
+        NodeLinkedList createdNode = new NodeLinkedList(data);
+
+        for (int i = 0; i < index - 1 ; i++) {
+           preNode = preNode.getNext();
+        }
+
+        createdNode.setNext(preNode.getNext());
+        preNode.setNext(createdNode);
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(5);
+        linkedList.append(16);
+        linkedList.append(17);
+        linkedList.append(18);
+        linkedList.insert(2,10);
+        linkedList.print();
+    }
     public void print(){
         NodeLinkedList currentNode = head;
 
