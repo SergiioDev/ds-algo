67bfa6a72b0c2e46bdd02800bd895c6a14bfcd30
SergiioDev <sergi13456@hotmail.es>
2022-10-03 18:48:40 +0200

    create Stack using Linked List

---
+++ src/main/java/datastructures/stacks/Stack.java
@@ -0,0 +1,56 @@
+package datastructures.stacks;
+
+import datastructures.linkedlists.singlylinkedlist.NodeLinkedList;
+
+public class Stack {
+    NodeLinkedList top;
+    NodeLinkedList bottom;
+    int length;
+
+    public Stack() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    public NodeLinkedList peek(){
+        return top;
+    }
+
+    public void push(int value){
+        NodeLinkedList element = new NodeLinkedList(value);
+
+        if (top == null) {
+            top = element;
+            bottom = top;
+
+        }else{
+            element.setNext(top);
+            top = element;
+        }
+        length++;
+    }
+
+
+    public void pop(){
+        if(length == 1){
+            bottom = null;
+        }
+        if(top != null){
+            top = top.getNext();
+            length--;
+        }
+
+    }
+
+    public void print(){
+        NodeLinkedList currentNode = top;
+
+        while(currentNode != null){
+            System.out.println(currentNode.getData());
+            currentNode = currentNode.getNext();
+        }
+
+    }
+
+}
