b8d6e346a75770de96364b6e6171cc92848d07fc
SergiioDev <sergi13456@hotmail.es>
2022-10-12 13:52:51 +0200

    create insertionSort method

--- src/main/java/algorithims/sorting/Sorting.java
+++ src/main/java/algorithims/sorting/Sorting.java
@@ -40,4 +40,29 @@ public class Sorting {
         return numbers;
     }
 
+
+    public static int[] insertionSort(int[] numbers){
+        for (int i = 0; i < numbers.length - 1 ; i++) {
+
+            if(numbers[i] > numbers[i+1]){
+                int temp = numbers[i];
+                numbers[i] = numbers[i+1];
+                numbers[i+1] = temp;
+            }
+
+            int j = i;
+
+            while ( j>0 &&  numbers[j] < numbers[j-1] ){
+                int temp = numbers[j-1];
+                numbers[j-1] = numbers[j];
+                numbers[j] = temp;
+                j--;
+            }
+        }
+        return numbers;
+    }
+
+
+
+
 }
