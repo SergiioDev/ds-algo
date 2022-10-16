d15ca45731c4e0ac85c29aa91306936ed4efbc97
SergiioDev <sergi13456@hotmail.es>
2022-10-16 19:03:47 +0200

    add leetcode solutions by topic

---
+++ src/main/java/leetocde/Strings.java
@@ -0,0 +1,19 @@
+package leetocde;
+
+public class Strings {
+
+    public static boolean isAnagram(java.lang.String s, java.lang.String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] store = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            store[s.charAt(i) - 'a']++;
+            store[t.charAt(i) - 'a']--;
+        }
+
+        for (int n : store) if (n != 0) return false;
+
+        return true;
+    }
+}
