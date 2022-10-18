6d859b54c5448bfffd9bc226ec01168aae672026
SergiioDev <sergi13456@hotmail.es>
2022-10-18 17:55:07 +0200

    refactor binarySearch

--- src/main/java/algorithims/search/Search.java
+++ src/main/java/algorithims/search/Search.java
@@ -2,20 +2,38 @@ package algorithims.search;
 
 public class Search {
 
-    public static int binarySearch(int[] nums, int left, int right, int target){
-        if (left > right){
-            return -1;
-        }
-        int mid = left + (right - 1)/2;
+    public static int binarySearch(int[] nums, int target) {
 
-        if (target > nums[mid]){
-          return  binarySearch(nums,mid + 1, right, target);
-        }else if(target < nums[mid]){
-           return binarySearch(nums,left, mid - 1, target);
-        }else{
-            return mid;
-        }
+        int left = 0, right = nums.length - 1;
 
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                return nums[mid];
+            }
+
+        }
+        return -1;
     }
 
+    public static boolean binarySearchRecursive(int[] nums, int left, int right, int target) {
+        if (left > right) {
+            return false;
+        }
+
+        int mid = (left + right) / 2;
+
+        if (target > nums[mid]) {
+            return binarySearchRecursive(nums, mid + 1, right, target);
+        } else if (target < nums[mid]) {
+            return binarySearchRecursive(nums, left, mid - 1, target);
+        } else {
+            return true;
+        }
+    }
 }
