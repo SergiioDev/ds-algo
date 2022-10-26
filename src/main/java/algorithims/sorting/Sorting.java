e01314cab636f5ed44564e6a2818b7283c5205af
SergiioDev <sergi13456@hotmail.es>
2022-10-26 10:28:32 +0200

    improve insertion sort algorithm

--- src/main/java/algorithims/sorting/Sorting.java
+++ src/main/java/algorithims/sorting/Sorting.java
@@ -45,16 +45,8 @@ public class Sorting {
 
     public static int[] insertionSort(int[] numbers) {
         for (int i = 0; i < numbers.length - 1; i++) {
-
-            if (numbers[i] > numbers[i + 1]) {
-                int temp = numbers[i];
-                numbers[i] = numbers[i + 1];
-                numbers[i + 1] = temp;
-            }
-
             int j = i;
-
-            while (j > 0 && numbers[j] < numbers[j - 1]) {
+            while (j >= 0 && numbers[j +1] < numbers[j]) {
                 int temp = numbers[j - 1];
                 numbers[j - 1] = numbers[j];
                 numbers[j] = temp;
