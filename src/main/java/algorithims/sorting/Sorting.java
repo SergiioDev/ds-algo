82d751cd1ed7d70d94a355d771c814f3d0130d43
SergiioDev <sergi13456@hotmail.es>
2022-10-27 15:38:50 +0200

    add bucketSort algorithm

--- src/main/java/algorithims/sorting/Sorting.java
+++ src/main/java/algorithims/sorting/Sorting.java
@@ -134,6 +134,23 @@ public class Sorting {
 
         return nums;
     }
+
+
+    public static int[] bucketSort(int[]nums){
+        int[] counts = {0,0,0};
+        for(int i =0; i< nums.length; i++){
+            counts[nums[i]]++;
+        }
+
+        int i = 0;
+        for(int n = 0; n < counts.length; n++){
+            for(int j = 0; j < counts[n]; j++){
+                nums[i++] = n;
+            }
+        }
+        return nums;
+    }
+
 }
 
 
