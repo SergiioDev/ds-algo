cbc7f3bc97412e49b2700a3402f553f1e19ae2e1
SergiioDev <sergi13456@hotmail.es>
2022-10-27 15:40:47 +0200

    add 73-Sort-Colors.java problem

---
+++ src/main/java/leetocde/medium/73-Sort-Colors.java
@@ -0,0 +1,18 @@
+package leetocde.medium;
+
+class SortColors {
+    public void sortColors(int[] nums) {
+        if (nums.length == 1) return;
+        int[] counts = {0,0,0};
+        for(int i =0; i< nums.length; i++){
+            counts[nums[i]]++;
+        }
+
+        int i = 0;
+        for(int n = 0; n < counts.length; n++){
+            for(int j = 0; j < counts[n]; j++){
+                nums[i++] = n;
+            }
+        }
+    }
+}
