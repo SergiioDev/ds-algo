ff437376434723b1802d0a693946b36909919732
SergiioDev <sergi13456@hotmail.es>
2022-11-02 10:34:46 +0100

    add 700-Search-In-a-Binary-Search-Tree-Recursive problem

---
+++ src/main/java/leetocde/easy/700-Search-In-a-Binary-Search-Tree-Recursive.java
@@ -0,0 +1,38 @@
+package leetocde.easy;
+
+class SearchInABinaryTreeRecursive {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (val > root.val) {
+            return searchBST(root.right, val);
+        } else if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return root;
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
